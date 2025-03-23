03a17572786862f1df39bb600cdc7409df1e8e37
freddy <freddy@fedora.minipc>
2025-03-23 20:50:39 +0100

    ex3_6 done

--- stock.py
+++ stock.py
@@ -1,12 +1,23 @@
 # stock.py
 
+
 class Stock:
-    __slots__ = ('name','_shares','_price')
+    __slots__ = ("name", "_shares", "_price")
     _types = (str, int, float)
+
     def __init__(self, name, shares, price):
         self.name = name
         self.shares = shares
         self.price = price
+
+    def __repr__(self):
+        return f"{type(self).__name__}({self.name!r}, {self.shares!r}, {self.price!r})"
+
+    def __eq__(self, other):
+        return isinstance(other, Stock) and (
+            (self.name, self.shares, self.price)
+            == (other.name, other.shares, other.price)
+        )
 
     @classmethod
     def from_row(cls, row):
@@ -16,23 +27,25 @@ class Stock:
     @property
     def shares(self):
         return self._shares
+
     @shares.setter
     def shares(self, value):
         if not isinstance(value, self._types[1]):
-            raise TypeError(f'Expected {self._types[1].__name__}')
+            raise TypeError(f"Expected {self._types[1].__name__}")
         if value < 0:
-            raise ValueError('shares must be >= 0')
+            raise ValueError("shares must be >= 0")
         self._shares = value
 
     @property
     def price(self):
         return self._price
+
     @price.setter
     def price(self, value):
         if not isinstance(value, self._types[2]):
-            raise TypeError(f'Expected {self._types[2].__name__}')
+            raise TypeError(f"Expected {self._types[2].__name__}")
         if value < 0:
-            raise ValueError('price must be >= 0')
+            raise ValueError("price must be >= 0")
         self._price = value
 
     @property
