a8357a8b26bc9bd5c320fdf4c87456c2d2e12008
freddy <freddy@fedora.minipc>
2025-03-23 20:25:40 +0100

    ex3_5 done

--- tableformat.py
+++ tableformat.py
@@ -1,17 +1,60 @@
 # tableformat.py
 
+
+class TableFormatter:
+    def headings(self, headers):
+        raise NotImplementedError()
+
+    def row(self, rowdata):
+        raise NotImplementedError()
+
+
+class TextTableFormatter(TableFormatter):
+    def headings(self, headers):
+        print(" ".join("%10s" % h for h in headers))
+        print(("-" * 10 + " ") * len(headers))
+
+    def row(self, rowdata):
+        print(" ".join("%10s" % d for d in rowdata))
+
+
+class CSVTableFormatter(TableFormatter):
+    def headings(self, headers):
+        print(",".join(headers))
+
+    def row(self, rowdata):
+        print(",".join(map(str, rowdata)))
+
+
+class HTMLTableFormatter(TableFormatter):
+    @staticmethod
+    def _tag(tag, content):
+        return f"<{tag}>{content}</{tag}>"
+
+    def headings(self, headers):
+        print(self._tag("tr", " ".join(self._tag("th", h) for h in headers)))
+
+    def row(self, rowdata):
+        print(self._tag("tr", " ".join(self._tag("td", r) for r in rowdata)))
+
+
+def create_formatter(kind):
+    match kind:
+        case "text":
+            return TextTableFormatter()
+        case "csv":
+            return CSVTableFormatter()
+        case "html":
+            return HTMLTableFormatter()
+        case _:
+            raise ValueError(
+                f"Unsupported formatter {kind}, must be one of text, csv, html"
+            )
+
+
 # Print a table
-def print_table(records, fields):
-    # Print the table headers in a 10-character wide field
-    for fieldname in fields:
-        print('%10s' % fieldname, end=' ')
-    print()
-
-    # Print the separator bars
-    print(('-'*10 + ' ')*len(fields))
-
-    # Output the table contents
+def print_table(records, fields, formatter):
+    formatter.headings(fields)
     for r in records:
-        for fieldname in fields:
-            print('%10s' % getattr(r, fieldname), end=' ')
-        print()
+        rowdata = [getattr(r, fieldname) for fieldname in fields]
+        formatter.row(rowdata)
